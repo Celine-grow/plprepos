cf832dacbf1feb12ba7c26442009544a345474f4
Celine-grow <celineloise4@gmail.com>
2024-02-23 19:53:57 +0300

    Python commit

--- user_input.py
+++ user_input.py
@@ -5,5 +5,5 @@ age=input("What is your age")
 
 location=input("Where do you stay i.e your location/place of residence")
 
-print(f"Hello {name},you are  {age} years old and you live in {location}")
+print(f"Hello {name},you are {age} years old and you live in {location}")
 
